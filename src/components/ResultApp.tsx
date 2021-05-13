381b848cb3169bcdd1c9a3fb7eb4286eef8001c6
lucasgio <mikaelosoltes@gmail.com>
2021-05-12 23:16:27 -0400

    Creating BookingApp,components and styles

---
+++ src/components/ResultApp.tsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+import Panel from "rsuite/lib/Panel";
+
+import "../css/style.css";
+
+export const ResultApp = () => {
+
+
+  return (
+    <Panel className="result_card" shaded>
+    </Panel>
+
+  ) 
+};
