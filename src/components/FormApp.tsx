855dd1d96e2e5efdff8257c283a2243c0d5dd52a
lucasgio <mikaelosoltes@gmail.com>
2021-05-15 02:26:22 -0400

    Test another UI

--- src/components/FormApp.tsx
+++ src/components/FormApp.tsx
@@ -1,64 +1,89 @@
-import React from "react";
-import { Button, DatePicker, FlexboxGrid, Form, InputPicker } from "rsuite";
-import FlexboxGridItem from "rsuite/lib/FlexboxGrid/FlexboxGridItem";
-// import { useFromSubmittion } from "../hooks/useFromSubmittion";
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  DatePicker,
+  FlexboxGrid,
+  Form,
+  FormControl,
+  FormGroup,
+  InputPicker,
+} from "rsuite";
 import { useGetSuggestion } from "../hooks/useGetSuggestion";
-
-
-
+import { FormType } from "../interface/GetCityType";
 
 const FormApp = () => {
+
+  const [data, setData] = useState<FormType>({
+
+    from:"",
+    to:"",
+    dateFrom:new Date(),
+    dateTo:new Date()
+
+  })  
   
-
-  const { fromCity,toCity,form,setForm } = useGetSuggestion();
-
+  const {dateFrom,dateTo} = data;
+  
+  
+  const { fromCity, toCity, form, setForm } = useGetSuggestion();
+  
+  const handleSubmite = (e:any) => {
+    e.preventDefault();  
+    console.log(dateFrom,dateTo)  
+  };
 
 
 
   return (
-      <Form >
-        <FlexboxGrid justify={'center'}>
-          <FlexboxGridItem className="selectStyle"> 
-              <InputPicker
+
+      <Container style={{ marginTop: "2rem" }}>
+        <FlexboxGrid justify="center">
+          <Form 
+            layout="inline"
+          >
+            <FormGroup>
+              <FormControl
+                name="from"
+                accepter={InputPicker}
                 data={fromCity}
                 onSearch={(searchKeyword: string)=>setForm({...form,from:searchKeyword})}
-               
+                style={{ width: 200 }}
               />
-          </FlexboxGridItem>
-          <FlexboxGridItem className="selectStyle"> 
-              <InputPicker
-                data      ={toCity}
+            </FormGroup>
+            <FormGroup>
+            <FormControl
+                name="to"
+                accepter={InputPicker}
+                data={toCity}
                 onSearch={(searchKeyword: string)=>setForm({...form,to:searchKeyword})}
-           
+                style={{ width: 200 }}
               />
-          </FlexboxGridItem>
-          <FlexboxGridItem className="selectStyle">
-              <DatePicker
-                style={{ width: 280 }} 
+            </FormGroup>
+            <FormGroup>
+              <FormControl
+                name="fromDate"
+                accepter={DatePicker}
                 format={'DD-MM-YYYY'}
                 placeholder={'Departure'}
-                limitEndYear={1}
-                showWeekNumbers={true}
-         
+                style={{ width: 200 }}
+                onChangeCalendarDate={(dateFrom:Date)=>setData({dateFrom:dateFrom})}
               />
-          </FlexboxGridItem>
-          <FlexboxGridItem className="selectStyle">
-              <DatePicker
-                style={{ width: 280 }} 
+            </FormGroup>
+            <FormGroup>
+              <FormControl
+                name="toDate"
+                accepter={DatePicker}
                 format={'DD-MM-YYYY'}
                 placeholder={'Return'}
-                limitEndYear={1}
-              
+                style={{ width: 200 }}
+                onChangeCalendarDate={(dateTo:Date)=>setData({dateTo:dateTo})}
               />
-          </FlexboxGridItem>
-          <Button
-            color={'green'}
-
-          >
-            Search
-          </Button>
+            </FormGroup>
+            <Button color={'green'} onClick={handleSubmite}>Search</Button>
+          </Form>
         </FlexboxGrid>
-      </Form>
+      </Container>
   );
 };
 
