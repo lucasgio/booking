b60ba4387c2bcce3aa0118e7708d69a75c7e5e55
lucasgio <mikaelosoltes@gmail.com>
2021-05-17 14:30:22 -0400

    Creating useContext

---
+++ src/interface/GetCityType.tsx
@@ -0,0 +1,27 @@
+export interface GetCityAPIType {
+    locations:         City[];
+}
+
+export interface City {
+    city:City;
+}
+
+export interface City {
+    id:                   string;
+    name:                 string;
+}
+
+
+export type ItemDataType = {
+    id:string;
+    label: string; 
+};
+
+export type FormType = {
+    from?:string;
+    to?:string;
+    dateFrom?:Date;
+    dateTo?:Date;
+}; 
+
+
